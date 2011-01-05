5f3b81abe8ddc011acdac930e0f633ff716685fa
Brandon Fulk <brandon.fulk@gmail.com>
2011-01-04 21:20:44 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gpanda::Application.config.secret_token = '5006b60e1dddeca68d5901dc458efef27c2df55889c05c1d5227db06e8aa225bbd69b56251d45a87772e9d623c6aba25803d4f73f503b960019b6ed4400f62c9'
